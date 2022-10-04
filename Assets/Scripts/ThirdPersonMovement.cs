93c582b17df602674f94ab9a528c642bbe145761
Kiran-505 <114230005+Kiran-505@users.noreply.github.com>
2022-10-04 20:35:31 +0200

    Integrated jumping and walking script + added walk / idle animations to cat

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     //motor that drives our player
-    public CharacterController controller;
+    private CharacterController controller;
     public Transform cam;
 
     public float speed = 6f;
+    public float animationSpeed = 5.0f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     //Jump speed and gravity
-    public float jumpSpeed;
-    private float ySpeed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public float fallingSpeed = -5f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Animator animator;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator.speed = animationSpeed;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check if player collides with the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        animator.SetBool("is_walking", false);
+
+        // if cat is on ground and is not jumping, then set the height to -1
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
         //move around using WASD and arrows keys
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -34,33 +57,37 @@ public class ThirdPersonMovement : MonoBehaviour
         //normalize : if we press down two keys at the same time so the speed is not doubled
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        //Adds physics to jumping
-        ySpeed += Physics.gravity.y * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump"))
+        //Adds physics to jumping
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            ySpeed = jumpSpeed;
+            velocity.y = Mathf.Sqrt(jumpHeight * -1.0f * gravity);
         }
+
 
         //if we're moving in any direction
         if(direction.magnitude >= 0.1f)
         {
-
+            // if cat is on ground then play walking animation
+            if (isGrounded)
+            {
+                animator.SetBool("is_walking", true);
+            }
             //rotating player towards the direction 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
             //smoothing numbers and angles
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             //rotation we move in account to the rotation of cam
             Vector3 moveDir= Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             //input to move
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
         }
-
-
     }
 }
