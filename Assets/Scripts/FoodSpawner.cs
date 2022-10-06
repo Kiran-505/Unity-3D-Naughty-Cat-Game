bf4f4a1a58657f395ddcc5dbb7816d79aa79ce2c
Kiran-505 <114230005+Kiran-505@users.noreply.github.com>
2022-10-06 17:55:13 +0200

    Added food locations and added food to spawn at those locations

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -7,8 +7,8 @@ public class FoodSpawner : MonoBehaviour
     public GameObject[] GoodFoods;
     public GameObject[] BadFoods;
 
-    private int goodFoodCount = 2;
-    private int badFoodCount = 0;
+    private int goodFoodCount = 10;
+    private int badFoodCount = 5;
 
     private List<Vector3> foodLocations = new List<Vector3>();
     private bool[] occupiedLocations;
@@ -28,7 +28,7 @@ public class FoodSpawner : MonoBehaviour
         SpawnFoodAtLocations(BadFoods, badFoodLocations);
     }
 
-    // Update is called once per frame
+    // Update is called once per framew
     void Update()
     {
     }
@@ -38,6 +38,19 @@ public class FoodSpawner : MonoBehaviour
         // Defining food locations
         foodLocations.Add(new Vector3(-2.856f, 1.526f, 5.722f));
         foodLocations.Add(new Vector3(-1.301f, 0.201f, 5.722f));
+        foodLocations.Add(new Vector3(4.4741f, 1.264f, 3.816f));
+        foodLocations.Add(new Vector3(5.397f, 0.2f, -4.872214f)); // closet near bedroom
+        foodLocations.Add(new Vector3(-6.067554f, 1.527f, -0.2253566f)); //computer table
+        foodLocations.Add(new Vector3(3.839324f, 0.265f, 7.453134f));// closet near kitchen
+        foodLocations.Add(new Vector3(2.918487f, 1.549f, -10.5524f)); //bathroom sink
+        foodLocations.Add(new Vector3(-4.732743f, 1.149f, -8.872142f)); //master bedroom bed
+        foodLocations.Add(new Vector3(-7.26672f, 1.138f, 6.847778f)); //small bedroom bed
+        foodLocations.Add(new Vector3(5.45f, 1.09109f, -0.08671708f)); //bench
+        foodLocations.Add(new Vector3(-0.197128f, 0.341f, -9.097137f)); //master bedroom closet
+        foodLocations.Add(new Vector3(-6.721f, 1.3278f, -4.52f)); //master bedroom cupboard
+        foodLocations.Add(new Vector3(-2.9821f, 0.91f, 6.733f)); //kitchen oven
+        foodLocations.Add(new Vector3(3.130547f, 0.852f, -10.616f)); //bathroom drawer
+        foodLocations.Add(new Vector3(-7.42775f, 0.274f, -7.982955f)); //master bedroom floor
 
         // Create a list of occupied locations equal to number of food locations
         occupiedLocations = new bool[foodLocations.Count];
