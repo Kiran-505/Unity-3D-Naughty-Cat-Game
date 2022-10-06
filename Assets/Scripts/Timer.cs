9f0fa59e4d25ca4692ea5aafb4ef9a58c21fb266
LinnLB96 <98334213+LinnLB96@users.noreply.github.com>
2022-10-06 20:40:13 +0200

    timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,12 +6,11 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    //Timer duration
     [SerializeField]
-    private float timerDuration = 1f * 30f; //Duration of the timer in seconds
+    private float timerDuration = 1f * 30f;
 
-    [SerializeField]
-    private bool countDown = true;
-
+    //For the diffrent text slots that will display the time
     private float timer;
     [SerializeField]
     private TextMeshProUGUI firstMinute;
@@ -24,98 +23,56 @@ public class Timer : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI secondSecond;
 
-    //Use this for a single text object
-    //[SerializeField]
-    //private TextMeshProUGUI timerText;
-
     private float flashTimer;
     [SerializeField]
     private float flashDuration = 1f; //The full length of the flash
 
     private void Start()
     {
-        ResetTimer();
+
     }
 
-    private void ResetTimer()
-    {
-        if (countDown)
-        {
-            timer = timerDuration;
-        }
-        else
-        {
-            timer = 0;
-        }
-        SetTextDisplay(true);
-    }
+
 
     void Update()
     {
-        if (countDown && timer > 0)
+        if (timer > 0)
         {
+            //Continues to countdown and display the time as long as its above 0 minutes left.
             timer -= Time.deltaTime;
-            UpdateTimerDisplay(timer);
-        }
-        else if (!countDown && timer < timerDuration)
-        {
-            timer += Time.deltaTime;
             UpdateTimerDisplay(timer);
         }
         else
         {
             FlashTimer();
+            
         }
     }
 
     private void UpdateTimerDisplay(float time)
     {
-        if (time < 0)
-        {
-            time = 0;
-        }
-
-        if (time > 3660)
-        {
-            Debug.LogError("Timer cannot display values above 3660 seconds");
-            ErrorDisplay();
-            return;
-        }
-
+        //rounds value down to make sure that it's really 0 and not 0.1/0.3 etc.
         float minutes = Mathf.FloorToInt(time / 60);
         float seconds = Mathf.FloorToInt(time % 60);
 
+        //Assigns characters to the string we want to display.
         string currentTime = string.Format("{00:00}{01:00}", minutes, seconds);
         firstMinute.text = currentTime[0].ToString();
         secondMinute.text = currentTime[1].ToString();
         firstSecond.text = currentTime[2].ToString();
         secondSecond.text = currentTime[3].ToString();
 
-        //Use this for a single text object
-        //timerText.text = currentTime.ToString();
     }
 
-    private void ErrorDisplay()
+    private void FlashTimer() //Checks time and makes sure that once the timer is 0 it flashes instead of continue below 0.
     {
-        firstMinute.text = "8";
-        secondMinute.text = "8";
-        firstSecond.text = "8";
-        secondSecond.text = "8";
-
-
-        //Use this for a single text object
-        //timerText.text = "ERROR";
-    }
-
-    private void FlashTimer()
-    {
-        if (countDown && timer != 0)
+        if (timer != 0)
         {
             timer = 0;
             UpdateTimerDisplay(timer);
         }
 
-        if (!countDown && timer != timerDuration)
+        if (timer != timerDuration)
         {
             timer = timerDuration;
             UpdateTimerDisplay(timer);
@@ -134,6 +91,7 @@ public class Timer : MonoBehaviour
         {
             flashTimer -= Time.deltaTime;
             SetTextDisplay(false);
+            Debug.Log("Game over");
         }
     }
 
@@ -144,8 +102,5 @@ public class Timer : MonoBehaviour
         separator.enabled = enabled;
         firstSecond.enabled = enabled;
         secondSecond.enabled = enabled;
-
-        //Use this for a single text object
-        //timerText.enabled = enabled;
     }
 }
